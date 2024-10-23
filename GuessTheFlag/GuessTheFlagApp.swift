e7f11ffa59ef02c82a6abdd53c9537a003fe3e46
Psyn3m <150781195+Psyn3m@users.noreply.github.com>
2024-10-23 09:09:29 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Némo Kardassevitch on 23/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
